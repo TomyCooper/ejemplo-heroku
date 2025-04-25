6fe1bd70d18cd8f232d29e70c98debccdd7ac01f
Tomas Robles <t.robles10@uniandes.edu.co>
2025-04-24 20:20:28 -0500

    se añade subtítulo

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Esta es una app desplegada en Heroku!</h1>'
+    return '<h1>Esta es una app desplegada en Heroku!</h1><br><h2>Un subtítulo casual</h2>'
 
 
 if __name__ == '__main__':
